4061da18bc535d37baf3ebb6f29e31435f7a685f
devasc <devasc@gmail.com>
2023-10-10 08:56:22 +0800

    2 - todo controller, todo model and todo crud routes

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Todo::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Todo::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $todo = Todo::find($id);
+        return $todo;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $todo = Todo::find($id);
+        $todo->update($request->all());
+        return $todo;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Todo::destroy($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// get todos list
+Route::get('/todos', [TodoController::class, 'index']);
+// retrieve a single todo
+Route::get('/todos/{id}', [TodoController::class, 'show']);
+// create a todo
+Route::post('/todos', [TodoController::class, 'store']);
+// update a todo
+Route::put('/todos/{id}', [TodoController::class, 'update']);
+// delete a todo
+Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
